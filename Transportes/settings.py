04ba9ed0a1fa7ed4e77a0ebf1de13d7f9b744cc3
Dyaxel <dyaxellopez20@gmail.com>
2026-04-13 16:11:45 -0600

    deploy

--- Transportes/settings.py
+++ Transportes/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Transportes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/dyaxe/Documents/python 2/Central de Transportes/Transportes/Transportes/templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
